37d7ca7107d9b301dee55820a615700208146dce
aleksey-shmatov <invalidreference@hotmail.com>
2015-09-15 19:59:55 +0300

    removed all references

--- index.d.ts
+++ index.d.ts
@@ -1,3 +1,2 @@
-/// <reference path="typings/tsd.d.ts" />
 export { observable, INotifyPropertyChanged, ObservableObject, PropertyChangeEvent, PropertyChangeInfo } from './lib/observable';
 export { ObservableCollection, INotifyCollectionChanged, CollectionChangeEvent, CollectionChangeInfo, CollectionChangeAction } from './lib/observableCollection';
