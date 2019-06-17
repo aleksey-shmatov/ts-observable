e97faaae8e470d2285d54c3d46ac71f48667903d
aleksey-shmatov <invalidreference@hotmail.com>
2019-06-17 23:16:12 +0400

    Minor fix

--- index.ts
+++
@@ -1,4 +0,0 @@
-export { observable, INotifyPropertyChanged, ObservableObject,
-	PropertyChangeEvent, PropertyChangeInfo } from './src/observable';
-export { ObservableCollection, INotifyCollectionChanged, CollectionChangeEvent,
-	CollectionChangeInfo, CollectionChangeAction} from './src/observableCollection';
---
+++ src/index.ts
@@ -0,0 +1,8 @@
+export {
+    observable, INotifyPropertyChanged,
+    PropertyChangeEvent, PropertyChangeInfo
+} from './observable';
+export {
+    ObservableCollection, INotifyCollectionChanged, CollectionChangeEvent,
+    CollectionChangeInfo, CollectionChangeAction
+} from './observableCollection';
